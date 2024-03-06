d997d705b046dc3d5deadd21c3d2cfd097673fd0
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-03-06 04:06:45 +0200

    final

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
 """
-Script to print hot posts on a given Reddit subreddit.
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
 
-def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/alyapany)"
-    }
-
-    params = {
-        "limit": 10
-    }
-
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-
-    if response.status_code == 404:
-        print("None")
-        return
-
-    results = response.json().get("data")
-
-    [print(c.get("data").get("title")) for c in results.get("children")]
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,40 +1,30 @@
 #!/usr/bin/python3
 """
-Module to fetch and print the titles of the first 10 hot posts for a given subreddit.
+Script to print hot posts on a given Reddit subreddit.
 """
 
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Function to retrieve and print the titles of the first 10 hot posts for a given subreddit.
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
 
-    Args:
-        subreddit: A string representing the name of the subreddit.
-
-    Returns:
-        None.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {
-        'User-Agent': 'python3:subreddit.subscriber.counter:v1.0 (by /user/alyapany)'}
-    response = requests.get(url, headers=headers)
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/alyapany)"
+    }
 
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
-    else:
-        print(None)
+    params = {
+        "limit": 10
+    }
 
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
-if __name__ == "__main__":
-    import sys
+    if response.status_code == 404:
+        print("None")
+        return
 
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        top_ten(subreddit)
+    results = response.json().get("data")
+
+    [print(c.get("data").get("title")) for c in results.get("children")]
