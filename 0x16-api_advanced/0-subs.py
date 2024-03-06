0060568bd473e6ad0da56554d5d1b5104946ff68
a7medalyapany <sci.ahmedibrahim76837@alexu.edu.eg>
2024-03-06 04:12:40 +0200

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,38 +1,14 @@
 #!/usr/bin/python3
-"""
-This module retrieves the number of subscribers for a subreddit from Reddit API.
-"""
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Retrieves the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit: A string representing the subreddit name.
-
-    Returns:
-        The number of subscribers of the subreddit. If the subreddit is invalid, returns 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "alyapany"}
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json().get('data')
-        if data:
-            return data.get('subscribers', 0)
-    return 0
-
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subscribers = number_of_subscribers(sys.argv[1])
-        print("{:d}".format(subscribers))
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
+                         rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
+    with requests.session() as client:
+        info = client.get(url, headers=headers, allow_redirects=False).json()
+        try:
+            return info.get('data', {}).get('subscribers', 0)
+        except Exception:
+            return 0
