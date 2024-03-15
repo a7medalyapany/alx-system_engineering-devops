23fa55fdc4bdcd7390a93ef3c321c4eba05f6139
a7medalyapany <103336732+a7medalyapany@users.noreply.github.com>
2024-03-15 04:07:08 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,7 @@
-# Fix for optimizing Nginx performance
-
-exec { 'fix--for-nginx':
-  command     => 'sed -i "s/sendfile on/sendfile off/" /etc/nginx/nginx.conf',
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  refreshonly => true,
-}
-
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => Exec['fix--for-nginx'],
-}
+events {}
+http {
+     keepalive_timeout 30s;
+     worker_processes auto;
+     client_body_buffer_size 8k;
+    client_header_buffer_size 1k;
+    proxy_buffer_size 16k;
